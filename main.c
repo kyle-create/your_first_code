c53133fa260ce5c2ba0e29d5703c54980d62456f
kyle-create <108158730+kyle-create@users.noreply.github.com>
2022-06-24 19:12:32 -0500

    Update main.c

--- main.c
+++ main.c
@@ -1,9 +1,34 @@
-int main()
-{
-  int i;
-  for (i = 0; i < 10; i++)
-  {
-    printf("School\n");
-  }
-  return 0;
-}
+   ("Hello,School\n");
+    ("Hello,School\n");
+    ("Hello,School\n");
+     ("Hello,School\n");
+    printf("Hello,School\n");
+     ("Hello,School\n");
+     ("Hello,School\n");
+   ("Hello,School\n");
+   ("Hello,School\n");
+   ("Hello,School\n");
+  ("Hello,School\n");
+   ("Hello,School\n");
+   ("Hello,School\n");
+   ("Hello,School\n");
+   ("Hello,School\n"); 
+       ("Hello,School\n");
+   ("Hello,School\n");
+   ("Hello,School\n");
+   ("Hello,School\n");
+   ("Hello,School\n");
+   ("Hello,School\n");
+   ("Hello,School\n");
+   ("Hello,School\n");
+   ("Hello,School\n");
+   ("Hello,School\n");
+   ("Hello,School\n");
+   ("Hello,School\n");
+   ("Hello,School\n");
+   ("Hello,School\n");
+   ("Hello,School\n");
+   ("Hello,School\n");
+   ("Hello,School\n");
+
+
